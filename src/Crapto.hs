41a0ade1bd8f359f7ea8222e783f4c8c38d69538
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-04-21 20:59:40 +0100

    tidy up

--- src/Crapto.hs
+++ src/Crapto.hs
@@ -30,31 +30,9 @@ upperAlphabet = ['A' .. 'Z']
 digits :: Alphabet
 digits = ['0' .. '9']
 
--- The following steps still have to be taken care of:
-
--- 1. Get the index of a letter within an alphabet
--- 2. Get the letter at a certain index within an alphabet
--- 3. Transform a string, character by character
-
-listLength :: [a] -> Int
-listLength [] = 0
-listLength (_ : xs) = 1 + listLength xs
-
 indexOf :: (Eq a) => a -> [a] -> Int
 indexOf _ [] = undefined
 indexOf a (x : xs) = if x == a then 0 else 1 + indexOf a xs
-
--- indexOf :: Char -> Alphabet -> Int
--- indexOf _ [] = undefined
--- indexOf ch (x:xs) = if x == ch then 0 else 1 + indexOf ch xs
-
-indexOfInt :: Int -> [Int] -> Int
-indexOfInt _ [] = undefined
-indexOfInt ch (x : xs) = if x == ch then 0 else 1 + indexOfInt ch xs
-
--- getIndex :: [a] -> Int -> a
--- getIndex [] _ = undefined
--- getIndex (x : xs) i = if i == 0 then x else getIndex xs (i - 1)
 
 alphabetRot :: Alphabet -> Int -> Char -> Char
 alphabetRot alphabet n ch = alphabet !! ((indexOf ch alphabet + n) `mod` length alphabet)
@@ -76,7 +54,7 @@ rotChar n ch
   | otherwise = ch
 
 rotFibChar :: Int -> Char -> Char
-rotFibChar n = rotChar (fib (n))
+rotFibChar n = rotChar (fib n)
 
 caesar :: Int -> Alphabet -> Alphabet
 caesar n = map (rotChar n)
@@ -89,14 +67,14 @@ rotFib msg =
   msg
     & S.fromList
     & S.mapWithIndex (\n c -> rotFibChar (n + 1) c)
-    & show -- this is bad  "fromList \"cqm\""
+    & show -- TODO this is bad  "fromList \"cqm\""
 
 unRotFib :: String -> String
 unRotFib msg =
   msg
     & S.fromList
     & S.mapWithIndex (\n c -> rotFibChar ((-n) - 1) c)
-    & show -- this is bad  "fromList \"cqm\""
+    & show -- TODO this is bad  "fromList \"cqm\""
 
 fib :: Int -> Int
 fib n = f n

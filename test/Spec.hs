932d0f45c31cce34bab677e1a7ed9db6084401ed
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-05-11 11:30:16 +0100

    add some tests using a mix of hspec and hedgehog

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,26 +1,47 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import qualified Crapto as C
 import qualified Data.List as List
 import Test.Hspec
 
 quote =
-  "No one is bored,\
-  \everything is boring\
-  \  - Mark Fisher"
+import qualified Data.Text as Text
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
 
-main :: IO ()
-main = hspec $ do
-  describe "Crapto" $ do
-    it "Should increment each letter alphabetically acording to the fibonacci sequence" $ do
-      let (_, _, secret) = C.encrapt "apl"
-      secret `shouldBe` "bqn"
+prop_reverse :: Property
+prop_reverse =
+  property $ do
+    xs <- forAll $ Gen.list (Range.linear 0 100) Gen.alpha
+    reverse (reverse xs) === xs
+
+prop_decrapt :: Property
+prop_decrapt =
+  property $ do
+    xs <- forAll $ Gen.string (Range.linear 0 100) Gen.alpha
+    let (_, _, secret) = C.encrapt xs
+        (_, _, plain) = C.decrapt secret
+    xs === plain
 
     it "Should reverse the operation" $ do
       let (_, _, secret) = C.decrapt "bqn"
       secret `shouldBe` "apl"
 
-    it "multiline text can be handled the same as lists of text using contRotFib" $ do
-      let (_, _, secret) = C.encrapt quote
-          (x : xs) = lines quote
-          -- not sure I trust this because where are the new lines???
-          (_, _, secret2) = List.foldr (\line (l, r, acc) -> let (a, b, secret) = C.contRotFib l r line in (a, b, acc <> secret)) (C.encrapt x) xs
-      secret `shouldBe` secret2
+tests :: IO Bool
+tests =
+  checkParallel $
+    Group
+      "Test.Example"
+      [ ("prop_reverse", prop_reverse)
+      , ("prop_decrapt", prop_decrapt)
+      ]
+
+main :: IO ()
+main = do
+  _results <- sequence [tests]
+  hspec $ do
+    describe "Crapto" $ do
+      it "Should increment each letter alphabetically acording to the fibonacci sequence" $ do
+        let (_, _, secret) = C.encrapt "apl"
+        secret `shouldBe` "bqn"

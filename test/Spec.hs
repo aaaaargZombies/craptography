83b26aba1fd5d4c0dc7af1edccf8563376165090
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-05-11 11:37:05 +0100

    clean up the merge

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,14 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import qualified Crapto as C
-import qualified Data.List as List
-import Test.Hspec
-
-quote =
-import qualified Data.Text as Text
 import Hedgehog
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
+import Test.Hspec
 
 prop_reverse :: Property
 prop_reverse =
@@ -23,10 +19,6 @@ prop_decrapt =
     let (_, _, secret) = C.encrapt xs
         (_, _, plain) = C.decrapt secret
     xs === plain
-
-    it "Should reverse the operation" $ do
-      let (_, _, secret) = C.decrapt "bqn"
-      secret `shouldBe` "apl"
 
 tests :: IO Bool
 tests =
@@ -45,3 +37,6 @@ main = do
       it "Should increment each letter alphabetically acording to the fibonacci sequence" $ do
         let (_, _, secret) = C.encrapt "apl"
         secret `shouldBe` "bqn"
+      it "Should reverse the operation" $ do
+        let (_, _, secret) = C.decrapt "bqn"
+        secret `shouldBe` "apl"

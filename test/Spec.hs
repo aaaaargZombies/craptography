ee684946c49aceb8870fd4f38be8f60eea5788dc
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-05-11 13:10:49 +0100

    WIP mocking stdin

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,7 +4,7 @@ import qualified Crapto as C
 import qualified Data.List as List
 import Test.Hspec
 
-quote =
+import qualified Data.Maybe as Maybe
 import qualified Data.Text as Text
 import Hedgehog
 import qualified Hedgehog.Gen as Gen
@@ -24,9 +24,19 @@ prop_decrapt =
         (_, _, plain) = C.decrapt secret
     xs === plain
 
-    it "Should reverse the operation" $ do
-      let (_, _, secret) = C.decrapt "bqn"
-      secret `shouldBe` "apl"
+prop_continue :: Property
+prop_continue =
+  property $ do
+    xs <- forAll $ Gen.string (Range.linear 0 10000) (Gen.choice [Gen.alpha, pure '\n'])
+    let
+      (_, _, single) = C.encrapt xs
+      (h, ts) = Maybe.fromMaybe (xs, []) $ List.uncons $ lines xs
+      (_, _, plural) =
+        List.foldr
+          (\line (l, r, acc) -> let (a, b, secret) = C.contRotFib l r ("\n" <> line) in (a, b, acc <> secret))
+          (C.encrapt h)
+          ts
+    single === plural
 
 tests :: IO Bool
 tests =
@@ -35,11 +45,13 @@ tests =
       "Test.Example"
       [ ("prop_reverse", prop_reverse)
       , ("prop_decrapt", prop_decrapt)
+      , ("prop_continue", prop_continue)
       ]
 
 main :: IO ()
 main = do
   _results <- sequence [tests]
+  _recheck <- recheckAt (Seed 3043689854464086461 6114734931481463403) "2:bCeF" prop_continue
   hspec $ do
     describe "Crapto" $ do
       it "Should increment each letter alphabetically acording to the fibonacci sequence" $ do
